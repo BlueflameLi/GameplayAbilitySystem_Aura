4de278f79f2dd38a241435439371754b87d6b141
BlueflameLi <58543957+BlueflameLi@users.noreply.github.com>
2025-03-04 17:47:08 +0800

    The Player State && Ability System Component and Attribute Set

---
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -0,0 +1,19 @@
+// Copyright Blueflame(Only test)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerState();
+};
