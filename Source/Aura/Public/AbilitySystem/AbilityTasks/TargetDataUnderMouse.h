2e516dc2932c52f20ca131e221f7236d2ffb3161
BlueflameLi <58543957+BlueflameLi@users.noreply.github.com>
2025-10-03 21:58:16 +0800

    Send Mouse Cursor Data

--- Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
+++ Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -6,7 +6,8 @@
 #include "Abilities/Tasks/AbilityTask.h"
 #include "TargetDataUnderMouse.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FVector&, Data);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FGameplayAbilityTargetDataHandle&, DataHandle);
+
 /**
  * 
  */
@@ -26,4 +27,5 @@ public:
 
 private:
 	virtual void Activate() override;
+	void SendMouseCursorData();
 };
